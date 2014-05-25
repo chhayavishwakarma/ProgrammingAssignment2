6854d14a2a45836c5082d68dfb565500d6684c4d
chhayavishwakarma <chhay.vish26@gmail.com>
2014-05-26 00:33:17 +0530

    done

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@
 ## makeCacheMatrix creates a special matrix object, and then cacheSolve
 ## calculates the inverse of the matrix.
 ## If the matrix inverse has already been calculated, it will instead
-## find it in the cache and return it, and not calculate it again.
+## find it in the cache and return it, and not calculate it again..
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
